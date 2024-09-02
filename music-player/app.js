b38d3cd536bf79591cf0cb48436f0882536e7a13
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-02 21:43:48 +0500

    Update music-player

--- music-player/app.js
+++ music-player/app.js
@@ -1,5 +1,6 @@
 let arr = [
     {songTitle: "Ye Jo Halka Halka", artistName: "NFAK", coverImg: "https://images.genius.com/2865831b5aac4db5f8a3a628f79464a2.682x682x1.jpg", songUrl: "https://audiofoc.bajao.pk/bajaostrm/320/46173.mp3?m=0&mp3=.mp3&startTime=1725287798&endTime=1725303998&token=vBGmD_kqcS5y-8vPG7-bH_XqSl4CceY5fUrZTXX78gA="},
+    {songTitle: "Kali Kali Zulfon", artistName: "NFAK", coverImg: "https://images.genius.com/2865831b5aac4db5f8a3a628f79464a2.682x682x1.jpg", songUrl: "https://audiofoc.bajao.pk/bajaostrm/320/46170.mp3?m=0&mp3=.mp3&startTime=1725287958&endTime=1725304158&token=hrbr5D1ipfI6rqkuPY2WVmb22UdTx2Tdu7jyQJC06ig="},
 ];
 
 // selectors
@@ -33,6 +34,7 @@ function main(){
 
     dynamicDataHandler.innerHTML = clutter;
     audio.src = arr[playingSong].songUrl;
+    console.log(audio.src)
     document.querySelector(".left").style.backgroundImage = `url(${arr[playingSong].coverImg})`;
 }
 main(); 
